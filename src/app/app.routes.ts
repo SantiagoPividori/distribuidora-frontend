f817266eeadfb92aaa427454b7d0b5b3049f405a
SantiagoPividori <pividorisantiago@gmail.com>
2026-02-21 22:07:57 -0300

    feat(routes): define application routes for authentication and dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "auth/login", loadComponent: () => import('./features/auth/login-page/login-page').then(m => m.LoginPage) },
+    { path: "auth/register", loadComponent: () => import('./features/auth/register-page/register-page').then(m => m.RegisterPage) },
+    { path: 'user/dashboard', loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)},
+    { path: '', pathMatch: 'full', redirectTo: "auth/login" }
+];
