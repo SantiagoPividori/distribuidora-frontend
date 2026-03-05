c3867067e323a093f6b9aa4262de8e555918546e
SantiagoPividori <pividorisantiago@gmail.com>
2026-03-05 06:22:40 -0300

    better now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: "auth/login", loadComponent: () => import('./features/auth/login-page/login-page').then(m => m.LoginPage) },
     { path: "auth/register", loadComponent: () => import('./features/auth/register-page/register-page').then(m => m.RegisterPage) },
-    { path: 'user/dashboard', loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)},
+    { path: 'user/dashboard', loadComponent: () => import('./features/users/pages/dashboard/dashboard.component').then(m => m.DashboardComponent)},
     { path: '', pathMatch: 'full', redirectTo: "auth/login" }
 ];
