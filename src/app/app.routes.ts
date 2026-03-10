73a8e0fb694ee759372f0bab0fa927d24c0a53bc
SantiagoPividori <pividorisantiago@gmail.com>
2026-03-10 16:21:45 -0300

    refactor(orders): update orders route to load OrdersOverviewComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -31,8 +31,8 @@ export const routes: Routes = [
     path: 'orders',
     canActivate: [authGuard],
     loadComponent: () =>
-      import('./features/orders/pages/order-list/order-list.component').then(
-        (m) => m.OrderListComponent,
+      import('./features/orders/pages/orders-overview/orders-overview.component').then(
+        (m) => m.OrdersOverviewComponent,
       ),
   },
   {
